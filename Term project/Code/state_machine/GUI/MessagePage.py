a021e9216863af4772fb2cd5329f1c3fbf91576d
Håkon Johansen Finstad <Haakonjf@stud.ntnu.no>
2023-04-25 16:15:35 +0200

    fixed chat

--- Term project/Code/state_machine/GUI/MessagePage.py
+++ Term project/Code/state_machine/GUI/MessagePage.py
@@ -5,34 +5,43 @@ class MessagePage():
 
     channels = []
     app = None
+    asd = None
+    messages = []
 
-    def __init__(self, app, channels):
+    def __init__(self, app : gui, channels):
         self.channels = channels
         self.app = app
         pass
 
-    def init(self):
-        with self.app.frame():
-            index = 0
+    def init(self, exitCallback):
+        index = 0
+        with self.app.frame("Messages", row = 0, column=0, rowspan=3):
             for i in self.channels:
                 self.app.addLabel(i, i, index, 0)
                 index += 1
                 self.app.addHorizontalSeparator(row=index, column=0, colspan=1, colour="gray")
                 index += 1
             
-            self.app.addVerticalSeparator(row=0, column=1, colspan=1, rowspan=index, colour="gray")
+        self.app.addVerticalSeparator(row=0, column=1, rowspan=index, colour="gray")
 
-            self.app.addNamedButton("Quit", "QuitFromMainView", None, row=0, column=3, colspan=1)
+        self.app.addNamedButton("Quit", "QuitFromMainView", self.temp, row=0, column=2, colspan=1)
+        self.app.entry("chatinput", "", row=3, column=2)
+        self.app.addButton("send", None, row=3, column=3)
 
-            with self.app.scrollPane("ScrollPane", row=1, column=2, colspan=2, rowspan=index-2, sticky="NEW", stretch="COLUMN"):
-                for x in range(10):
-                    with self.app.labelFrame("message{number}".format(number=x), stretch="COLUMN", sticky="NEW", colspan=2, column=0, row=x):
-                        self.app.setSticky("NEW")
-                        self.app.setStretch("COLUMN")
-                        self.app.addLabel("l{number}".format(number=x), "Sender", column=0, row= 0)
-                        self.app.addMessage("l{number}".format(number=x), "heiadsaoidjaodijsdoisajdsaoidjsaoidjsaoidsadoisadjoiajd jk aksjd asidj asoid jaoidjoisajdosajdosa jaoi joisa", column=1, row=1)
-                        self.app.setMessageBg("l{number}".format(number=x), "khaki")
-
-
-            self.app.entry("chatinput", "", index-1, 2)
-            self.app.addButton("send", None, index-1, 3)
+    def temp(self):
+        self.show_message({"sender" : "Haakon", "message" : "hei"})
+        
+    def show_message(self, message):
+        self.messages.append(message)
+        if self.asd != None:
+            self.app.removeWidgetType(48, "ScrollPane")
+            
+        with self.app.scrollPane("ScrollPane", row=1, column=2, colspan=2, rowspan=1, sticky="NEW", stretch="COLUMN") as self.asd:
+            for index in range(len(self.messages)):
+                with self.app.labelFrame("message{number}".format(number=index), stretch="COLUMN", sticky="NEW", colspan=2, column=0, row=index):
+                    self.app.setSticky("NEW")
+                    self.app.setStretch("COLUMN")
+                    self.app.addLabel("l{number}".format(number=index), self.messages[index]["sender"], column=0, row= 0)
+                    self.app.addMessage("l{number}".format(number=index), self.messages[index]["message"], column=1, row=1)
+                    self.app.setMessageBg("l{number}".format(number=index), "khaki")
+               
